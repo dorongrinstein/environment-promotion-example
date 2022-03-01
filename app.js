15375c1da81b62711f3505ac17b07cef9d4d4a09
Doron Grinstein <dorongrinstein@hotmail.com>
2022-02-28 20:08:27 -0800

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 8080
 
 express()
     .get('/', (req, res) => {
-        res.send('Hello World!! the time is 8:55am');
+        res.send('Hello World!! I am first!');
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
